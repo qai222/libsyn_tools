34dc2319e5ba08c7edad429e28454274e648f87e
qai222 <qai@mit.edu>
2024-06-24 19:44:43 -0400

    add operation classes to init

--- libsyn_tools/chem_schema/__init__.py
+++ libsyn_tools/chem_schema/__init__.py
@@ -1,5 +1,6 @@
 from .base import Entity
 from .chemical import Chemical
+from .operation import Operation, FunctionalModule, OperationType
 from .reaction import ChemicalReaction, ChemicalReactionSpecificationLevel
 from .network_reaction import ReactionNetwork
 from .network_operation import OperationGraph, OperationNetwork
