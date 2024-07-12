c6e00b460801f7be40832c970428a6193acd2bdf
qai222 <qai@mit.edu>
2024-07-12 14:00:34 -0400

    visualize source from output to solver

--- lst_app/pages/scheduler.py
+++ lst_app/pages/scheduler.py
@@ -7,7 +7,7 @@ from dash import html, get_app, Input, Output, no_update, dcc, State
 from dash import register_page
 
 from libsyn_tools.chem_schema import ReactionNetwork, Chemical, ChemicalReaction, OperationNetwork
-from libsyn_tools.opt import SchedulerOutput, SchedulerInput
+from libsyn_tools.opt import SchedulerOutput, SchedulerInput, SolverMILP, SolverBaseline
 from libsyn_tools.utils import json_load, FilePath, CytoNodeData, is_uuid
 
 # page setup and constants
@@ -16,8 +16,8 @@ app = get_app()
 cyto.load_extra_layouts()
 THIS_DIR = os.path.dirname(os.path.abspath(__file__))
 PAGE_ID_HEADER = "Scheduler__"  # ID header for components in this page
-TARGET_JSON0 = "scheduler_output.json"
-TARGET_JSON1 = "scheduler_input.json"
+TARGET_JSON0 = "solver_milp.json"
+TARGET_JSON1 = "solver_baseline.json"
 TARGET_JSON2 = "operation_network.json"
 TARGET_JSON3 = "reaction_network.json"
 routes_folders = sorted(glob.glob(f"{THIS_DIR}/../routes/*"))
@@ -35,51 +35,71 @@ component_selector_available_routes = dbc.Select(
 # TODO decide if we really need session storage
 
 # cytoscape canvas for the network
-component_gantt_id = PAGE_ID_HEADER + "gantt"
-component_gantt = dcc.Graph(id=component_gantt_id)
+component_gantt_milp_id = PAGE_ID_HEADER + "gantt_milp"
+component_gantt_milp = dcc.Graph(id=component_gantt_milp_id)
+component_gantt_baseline_id = PAGE_ID_HEADER + "gantt_baseline"
+component_gantt_baseline = dcc.Graph(id=component_gantt_baseline_id)
 
 # callback for visualize the network in cytoscape
 @app.callback(
-    Output(component_gantt_id, 'figure'),
+    Output(component_gantt_milp_id, 'figure'),
+    Output(component_gantt_baseline_id, 'figure'),
     Input(component_selector_available_routes_id, 'value'),
 )
-def visualize_scheduler_output(folder: FilePath) -> go.Figure:
+def visualize_scheduler_output(folder: FilePath) -> tuple[go.Figure, go.Figure]:
     if not folder:
-        fig = go.Figure(go.Scatter(x=[], y=[]))
-        fig.update_layout(template=None)
-        fig.update_xaxes(showgrid=False, showticklabels=False, zeroline=False)
-        fig.update_yaxes(showgrid=False, showticklabels=False, zeroline=False)
-        return fig
-    scheduler_output_path = os.path.join(folder, TARGET_JSON0)
-    scheduler_input_path = os.path.join(folder, TARGET_JSON1)
+        fig_milp = go.Figure(go.Scatter(x=[], y=[]))
+        fig_milp.update_layout(template=None)
+        fig_milp.update_xaxes(showgrid=False, showticklabels=False, zeroline=False)
+        fig_milp.update_yaxes(showgrid=False, showticklabels=False, zeroline=False)
+        return fig_milp, fig_milp
+    solver_milp_path = os.path.join(folder, TARGET_JSON0)
+    solver_baseline_path = os.path.join(folder, TARGET_JSON1)
     operation_network_path = os.path.join(folder, TARGET_JSON2)
     reaction_network_path = os.path.join(folder, TARGET_JSON3)
 
-    scheduler_output, scheduler_input, operation_network, reaction_network = json_load(scheduler_output_path), json_load(scheduler_input_path), json_load(operation_network_path), json_load(reaction_network_path)
+    solver_milp, solver_baseline, operation_network, reaction_network = json_load(solver_milp_path), json_load(solver_baseline_path), json_load(operation_network_path), json_load(reaction_network_path)
 
-    scheduler_output = SchedulerOutput(**scheduler_output)
-    scheduler_input = SchedulerInput(**scheduler_input)
+    solver_milp = SolverMILP(**solver_milp)
+    solver_baseline = SolverBaseline(**solver_baseline)
     operation_network = OperationNetwork(**operation_network)
     reaction_network = ReactionNetwork(**reaction_network)
 
-    fmid2fm = {fm.identifier: fm for fm in scheduler_input.functional_modules}
+    fmid2fm = {fm.identifier: fm for fm in solver_baseline.input.functional_modules}
 
-    fig = go.Figure(layout=go.Layout(margin={"t": 0}))
+    fig_milp = go.Figure(layout=go.Layout(margin={"t": 0}))
     for rid, operations in operation_network.operations_by_reaction.items():
         y = []
         x = []
         base = []
         for operation in operations:
-            fmid = scheduler_output.assignments[operation.identifier]
+            fmid = solver_milp.output.assignments[operation.identifier]
             fm = fmid2fm[fmid]
             y.append(fm.name)
-            s = scheduler_output.start_times[operation.identifier]
-            e = scheduler_output.end_times[operation.identifier]
+            s = solver_milp.output.start_times[operation.identifier]
+            e = solver_milp.output.end_times[operation.identifier]
             base.append(s)
             x.append(e - s)
         bar = go.Bar(base=base, x=x, y=y, orientation='h', name=rid)
-        fig.add_trace(bar)
-    return fig
+        fig_milp.add_trace(bar)
+
+
+    fig_baseline = go.Figure(layout=go.Layout(margin={"t": 0}))
+    for rid, operations in operation_network.operations_by_reaction.items():
+        y = []
+        x = []
+        base = []
+        for operation in operations:
+            fmid = solver_baseline.output.assignments[operation.identifier]
+            fm = fmid2fm[fmid]
+            y.append(fm.name)
+            s = solver_baseline.output.start_times[operation.identifier]
+            e = solver_baseline.output.end_times[operation.identifier]
+            base.append(s)
+            x.append(e - s)
+        bar = go.Bar(base=base, x=x, y=y, orientation='h', name=rid)
+        fig_baseline.add_trace(bar)
+    return fig_milp, fig_baseline
 
 
 # information panel
@@ -101,7 +121,7 @@ layout = dbc.Container(
                 html.Hr(),
                 component_information_panel,
             ], className="col-12"),
-            dbc.Col([component_gantt], className="col-12"),
+            dbc.Col([component_gantt_milp, component_gantt_baseline], className="col-12"),
         ]
     ),
     # style={"width": "calc(100vw - 100px)"}
