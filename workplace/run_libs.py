f052a3621da5c8b68a67fc3dfde36b9f2a4fde2a
qai222 <qai@mit.edu>
2024-07-26 03:35:32 -0400

    setup for molgpu run

--- workplace/run_libs.py
+++ workplace/run_libs.py
@@ -24,41 +24,42 @@ def _get_fms(module_settings):
     return fms
 
 
-FMS_1 = _get_fms(
+FMS_0 = _get_fms(
     module_settings={
         OperationType.Heating: (1, 2),  # capacity, module_number, default to be (1, 1)
-        OperationType.Purification: (1, 2),
-        OperationType.TransferLiquid: (1, 2),
-        OperationType.TransferSolid: (1, 2),
-        OperationType.Concentration: (1, 2),
+        OperationType.TransferLiquid: (1, 1),
+        OperationType.MakeSolution: (1, 2),
+        OperationType.ConcentrationAndPurification: (1, 2),
+    },
+)
+
+FMS_1 = _get_fms(
+    module_settings={
+        OperationType.Heating: (2, 2),  # capacity, module_number, default to be (1, 1)
+        OperationType.TransferLiquid: (1, 1),
+        OperationType.MakeSolution: (1, 2),
+        OperationType.ConcentrationAndPurification: (1, 2),
     },
 )
 
 FMS_2 = _get_fms(
     module_settings={
         OperationType.Heating: (3, 2),  # capacity, module_number, default to be (1, 1)
-        OperationType.Purification: (1, 2),
-        OperationType.TransferLiquid: (1, 2),
-        OperationType.TransferSolid: (1, 2),
-        OperationType.Concentration: (1, 2),
+        OperationType.TransferLiquid: (1, 1),
+        OperationType.MakeSolution: (1, 2),
+        OperationType.ConcentrationAndPurification: (1, 2),
     },
 )
 
-FMS_3 = _get_fms(
-    module_settings={
-        OperationType.Heating: (3, 3),  # capacity, module_number, default to be (1, 1)
-        OperationType.Purification: (1, 1),
-        OperationType.TransferLiquid: (1, 2),
-        OperationType.TransferSolid: (1, 2),
-        OperationType.Concentration: (1, 1),
-    },
-)
-
-FMS_LIST = [FMS_1, FMS_2, FMS_3]
+FMS_LIST = [
+    FMS_0, FMS_1, FMS_2,
+]
 
 
 def run_one(runs_folder: FilePath, libs_folder: FilePath, prefix: str, x: int, y: int, fms_index: int,
-            has_work_shifts: bool, temperature_threshold: float, time_limit: int, gb_threads: int):
+            has_work_shifts: bool,
+            # temperature_threshold: float,
+            time_limit: int, gb_threads: int):
     fms = FMS_LIST[fms_index]
 
     lib_dir = os.path.join(libs_folder, f"{prefix}-{x:02}-{y:02}")
@@ -73,8 +74,7 @@ def run_one(runs_folder: FilePath, libs_folder: FilePath, prefix: str, x: int, y
     workflow = Workflow(routes_file="", work_folder=run_dir)
 
     json_dump([fm.model_dump() for fm in fms], os.path.join(run_dir, workflow.functional_modules_json))
-    workflow.export_scheduler_input(rng=random.Random(42), temperature_threshold=temperature_threshold,
-                                    dummy_work_shifts=has_work_shifts)
+    workflow.export_scheduler_input(rng=random.Random(42), dummy_work_shifts=has_work_shifts)
     workflow.export_solver(baseline=False, time_limit=time_limit, gb_threads=gb_threads)
     workflow.export_solver(baseline=True, time_limit=time_limit, gb_threads=gb_threads)
 
@@ -82,16 +82,20 @@ def run_one(runs_folder: FilePath, libs_folder: FilePath, prefix: str, x: int, y
 if __name__ == '__main__':
     RUNS_FOLDER = os.path.join(os.getcwd(), "RUNS")
     LIBS_FOLDER = os.path.join(os.getcwd(), "LIBS")
-    PREFIX = "FDA"
-    TEMPERATURE_THRESHOLD = 30
-    TIME_LIMIT = 3600
-    GB_THREADS = 48
+    assert os.path.exists(LIBS_FOLDER)
+    os.makedirs(RUNS_FOLDER, exist_ok=True)
 
-    X_MAX = 4
+    PREFIX = "FDA"
+    TIME_LIMIT = 3600
+    GB_THREADS = 64
+
+    X_MIN = 1
+    X_MAX = 10
+    Y_MIN = 1
     Y_MAX = 10
 
-    for X in range(3, X_MAX + 1):
-        for Y in range(1, Y_MAX + 1):
+    for X in range(X_MIN, X_MAX + 1):
+        for Y in range(Y_MIN, Y_MAX + 1):
             for IFM in range(len(FMS_LIST)):
                 run_one(RUNS_FOLDER, LIBS_FOLDER, PREFIX, X, Y, IFM, has_work_shifts=False,
-                        temperature_threshold=TEMPERATURE_THRESHOLD, time_limit=TIME_LIMIT, gb_threads=GB_THREADS)
+                        time_limit=TIME_LIMIT, gb_threads=GB_THREADS)
