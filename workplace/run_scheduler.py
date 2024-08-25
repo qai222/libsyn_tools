34890e41ba96ff3b3ec34fbce6a28d0c0165f0a2
qai222 <qai@mit.edu>
2024-08-24 21:43:24 -0400

    add baseline only option

--- workplace/run_scheduler.py
+++ workplace/run_scheduler.py
@@ -60,7 +60,9 @@ FMS_LIST = [
 def run_one(runs_folder: FilePath, libs_folder: FilePath, prefix: str, x: int, y: int, fms_index: int,
             has_work_shifts: bool,
             # temperature_threshold: float,
-            time_limit: int, gb_threads: int):
+            time_limit: int, gb_threads: int,
+            baseline_only: bool
+            ):
     fms = FMS_LIST[fms_index]
 
     lib_dir = os.path.join(libs_folder, f"{prefix}-{x:02}-{y:02}")
@@ -78,7 +80,8 @@ def run_one(runs_folder: FilePath, libs_folder: FilePath, prefix: str, x: int, y
 
     json_dump([fm.model_dump() for fm in fms], os.path.join(run_dir, workflow.functional_modules_json))
     workflow.export_scheduler_input(rng=random.Random(42), dummy_work_shifts=has_work_shifts)
-    workflow.export_solver(baseline=False, time_limit=time_limit, gb_threads=gb_threads)
+    if not baseline_only:
+        workflow.export_solver(baseline=False, time_limit=time_limit, gb_threads=gb_threads)
     workflow.export_solver(baseline=True, time_limit=time_limit, gb_threads=gb_threads)
 
 
@@ -92,6 +95,7 @@ parser.add_argument('--runs-folder', type=str, default="RUNS", help='runs folder
 parser.add_argument('--libs-folder', type=str, default="LIBS", help='libs folder path')
 parser.add_argument('--fms-index', type=int, help='index of the functional module set')
 parser.add_argument('--work-shift', action="store_true", help='if include work shifts')
+parser.add_argument('--baseline-only', action="store_true", help='if only run the baseline scheduler')
 
 args = parser.parse_args()
 
@@ -107,6 +111,7 @@ def main():
         has_work_shifts=args.work_shift,
         time_limit=args.gb_timelimit,
         gb_threads=args.gb_threads,
+        baseline_only=args.baseline_only,
     )
 
 
