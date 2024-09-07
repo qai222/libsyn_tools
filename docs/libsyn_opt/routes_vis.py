1a0ec958b7e80b49e66bc50ac12340503dda7b46
qai222 <qai@mit.edu>
2024-09-07 09:01:45 -0400

    better routes vis

--- docs/libsyn_opt/routes_vis.py
+++ docs/libsyn_opt/routes_vis.py
@@ -1,4 +1,5 @@
 import os
+import urllib.parse
 import xml.etree.ElementTree as ET
 from io import BytesIO
 
@@ -6,29 +7,19 @@ import dash_bootstrap_components as dbc
 import dash_cytoscape as cyto
 import networkx as nx
 import requests
-from dash import Dash, Output, Input, html, ctx, dcc, callback
+from dash import Dash, Output, Input, html, ctx, callback
 
 from libsyn_tools.chem_schema import ReactionNetwork
-from libsyn_tools.utils import json_load, CytoNodeData, drawing_url, CytoNode, CytoEdge, CytoEdgeData
+from libsyn_tools.utils import json_load, CytoNodeData, drawing_url, CytoNode, CytoEdge, CytoEdgeData, FilePath
 
 """
+visualize the synthetic routes of a library
+
 download example see 
 https://dash.plotly.com/cytoscape/images
 """
 
-
-app = Dash(
-    name=__name__,
-    title="LibSyn",
-    external_stylesheets=[dbc.themes.BOOTSTRAP, dbc.icons.FONT_AWESOME],
-)
-server = app.server
 cyto.load_extra_layouts()
-RUNS_FOLDER = "../../workplace/RUNS"
-RUNS_FOLDER = os.path.abspath(RUNS_FOLDER)
-RUN_NAME = "FDA-03-09-0-0"
-# RUN_NAME = "VS-07-10-0-0"
-
 STYLE_SHEET = [
     {
         # global style for edges
@@ -92,14 +83,6 @@ STYLE_SHEET = [
 ]
 
 
-def load_reaction_network() -> ReactionNetwork:
-    run_folder = os.path.join(RUNS_FOLDER, RUN_NAME)
-    assert os.path.isdir(run_folder)
-    reaction_network = os.path.join(run_folder, "reaction_network.json")
-    reaction_network = ReactionNetwork(**json_load(reaction_network))
-    return reaction_network
-
-
 def get_routes_graph(reaction_network: ReactionNetwork):
     """
     routes graph is a nx digraph that contains only intermediates, targets, and starting materials from leaf reactions
@@ -140,30 +123,75 @@ def get_routes_graph(reaction_network: ReactionNetwork):
     return rng, target_nodes, intermediate_nodes, leaf_reactions_main_reactants
 
 
+def get_routes_vis_app(
+        runs_folder: FilePath,
+        run_name: str
+):
+    run_folder = os.path.join(runs_folder, run_name)
+    assert os.path.isdir(run_folder)
+    reaction_network = os.path.join(run_folder, "reaction_network.json")
+    reaction_network = ReactionNetwork(**json_load(reaction_network))
+    component_cytoscape = cyto.Cytoscape(
+        id='cytoscape-image-export',
+        wheelSensitivity=0.01,
+        layout={
+            'name': 'dagre',
+            'nodeDimensionsIncludeLabels': True,
+            'animate': True,
+            'animationDuration': 1000,
+            'rankDir': 'LR',
+            'align': 'UL',
+        },
+        # style={'width': '100%', 'height': '100%'},  # browser exploded when setting height
+        className="border-primary border w-100",
+        responsive=True,
+        stylesheet=STYLE_SHEET,
+        elements=get_routes_graph_cyto(reaction_network)
+    )
+
+    app = Dash(
+        name=__name__,
+        title="LibSyn",
+        external_stylesheets=[dbc.themes.BOOTSTRAP, dbc.icons.FONT_AWESOME],
+    )
+
+    app.layout = html.Div([
+        html.Div(children=[
+            component_cytoscape
+        ]),
+
+        html.Div(children=[
+            html.Div('Download graph:'),
+            html.Button("as svg", id="btn-get-svg"),
+        ])
+    ])
+
+    return app
+
+
 def get_routes_graph_cyto(reaction_network: ReactionNetwork):
     routes_rng, target_nodes, intermediate_nodes, leaf_nodes = get_routes_graph(reaction_network)
     nodes = []
     edges = []
 
-
     reaction_ids = sorted([cr.identifier for cr in reaction_network.chemical_reactions])
     reaction_indexer = {
         cr: ii for ii, cr in enumerate(reaction_ids)
     }
-    for rid, ii in reaction_indexer.items():
-        print(ii, rid, reaction_network.entity_dictionary[rid].reaction_smiles)
+    # for rid, ii in reaction_indexer.items():
+    #     print(ii, rid, reaction_network.entity_dictionary[rid].reaction_smiles)
 
     product_to_reaction_index = dict()
     reactant_to_reaction_index = dict()
     for reaction in reaction_network.chemical_reactions:
-        product_to_reaction_index[reaction.product_smiles]= reaction_indexer[reaction.identifier]
+        product_to_reaction_index[reaction.product_smiles] = reaction_indexer[reaction.identifier]
         for r in reaction.reactants:
             reactant_to_reaction_index[r.smiles] = reaction_indexer[reaction.identifier]
 
     for idx, n in enumerate(sorted(routes_rng.nodes)):
         node_data = CytoNodeData(id=n, label=n, )
 
-        smi_url = drawing_url(smiles=n, size=80)
+        smi_url = drawing_url(smiles=urllib.parse.quote(n), size=80)
         response = requests.get(smi_url)
         svg_content = BytesIO(response.content)
         tree = ET.parse(svg_content)
@@ -203,42 +231,10 @@ def get_routes_graph_cyto(reaction_network: ReactionNetwork):
     return nodes + edges
 
 
-
-component_cytoscape = cyto.Cytoscape(
-    # id="component_cytoscape",
-    id='cytoscape-image-export',
-    wheelSensitivity=0.01,
-    layout={
-        'name': 'dagre',
-        'nodeDimensionsIncludeLabels': True,
-        'animate': True,
-        'animationDuration': 1000,
-        'rankDir': 'LR',
-        'align': 'UL',
-    },
-    # style={'width': '100%', 'height': '100%'},  # browser exploded when setting height
-    className="border-primary border w-100",
-    responsive=True,
-    stylesheet=STYLE_SHEET,
-    elements=get_routes_graph_cyto(load_reaction_network())
-)
-
-app.layout = html.Div([
-    html.Div(children=[
-        component_cytoscape
-    ]),
-
-    html.Div(children=[
-        html.Div('Download graph:'),
-        html.Button("as svg", id="btn-get-svg"),
-    ])
-])
-
 @callback(
     Output("cytoscape-image-export", "generateImage"),
-        Input("btn-get-svg", "n_clicks"),)
+    Input("btn-get-svg", "n_clicks"), )
 def get_image(get_svg_clicks):
-
     # File type to output of 'svg, 'png', 'jpg', or 'jpeg' (alias of 'jpg')
 
     # 'store': Stores the image data in 'imageData' !only jpg/png are supported
@@ -258,4 +254,13 @@ def get_image(get_svg_clicks):
 
 
 if __name__ == '__main__':
-    app.run(port=8064, debug=True)
+    RUNS_FOLDER = "../../workplace/RUNS"
+    RUNS_FOLDER = os.path.abspath(RUNS_FOLDER)
+    # RUN_NAME = "FDA-03-09-0-0"
+    # RUN_NAME = "VS-07-01-1-1"
+    RUN_NAME = "VS-04-06-1-1"
+    app = get_routes_vis_app(
+        runs_folder=RUNS_FOLDER,
+        run_name=RUN_NAME
+    )
+    app.run(port=8064, debug=False)
