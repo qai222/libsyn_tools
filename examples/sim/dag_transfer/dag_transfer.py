c48d63051210362dad1308ac02b0a4bac9ca6e8e
Qianxiang Ai <qianxiang.ai@abstraxtech.com>
2025-07-03 16:27:58 -0400

    refactor dag transfer example

--- examples/sim/dag_transfer/dag_transfer.py
+++ examples/sim/dag_transfer/dag_transfer.py
@@ -3,8 +3,9 @@ import random
 from loguru import logger
 
 from libsyn_tools.chem_schema import Chemical
-from libsyn_tools.sim import LabObject, KnowledgeGraph, PortionOfMaterial, UnitaryEdit, UnitaryEditType, \
-    TransferMaterialByPortionSize, ActionSimulation
+from libsyn_tools.sim import LabObject, KnowledgeGraph, PortionOfMaterial, Simulation, \
+    Create, MaterialContainer
+from libsyn_tools.sim.operation_preset import TransferMaterialByPortionSize
 
 """
 This example is a simulation of multiple transfer actions with the following precedence relations
@@ -22,16 +23,16 @@ def init_world():
     by applying CREATE edits to the global KnowledgeGraph.
     """
     logger.info("Initializing the world.")
-    beaker_1 = LabObject(identifier="beaker_1")
-    beaker_2 = LabObject(identifier="beaker_2")
-    beaker_3 = LabObject(identifier="beaker_3")
-    beaker_4 = LabObject(identifier="beaker_4")
+    beaker_1 = MaterialContainer(identifier="beaker_1")
+    beaker_2 = MaterialContainer(identifier="beaker_2")
+    beaker_3 = MaterialContainer(identifier="beaker_3")
+    beaker_4 = MaterialContainer(identifier="beaker_4")
     pipette_1 = LabObject(identifier="pipette_1")
     pipette_2 = LabObject(identifier="pipette_2")
 
     # Create a chemical portion for water
-    for lab_object in [beaker_1, beaker_2, beaker_3, beaker_4, pipette_1, pipette_2]:
-        UnitaryEdit(type=UnitaryEditType.CREATE, instance_1_iri=lab_object.instance_iri).apply()
+    for obj in [beaker_1, beaker_2, beaker_3, beaker_4, pipette_1, pipette_2]:
+        Create(instance_1_iri=obj.instance_iri).apply()
 
     for beaker in [beaker_1, beaker_2, beaker_3, beaker_4]:
         water = Chemical.make_up_from_smiles("O")  # e.g., H2O
@@ -39,7 +40,7 @@ def init_world():
         water_pom = PortionOfMaterial()
         water_pom.add_chemical(water)
         water_pom.is_directly_contained_by.add(beaker)
-        UnitaryEdit(type=UnitaryEditType.CREATE, instance_1_iri=water_pom.instance_iri).apply()
+        Create(instance_1_iri=water_pom.instance_iri).apply()
 
     return beaker_1, beaker_2, beaker_3, beaker_4, pipette_1, pipette_2
 
@@ -47,9 +48,9 @@ def init_world():
 def get_transfer(index, precedents, source, target, pipette, portion_size, time_cost):
     return TransferMaterialByPortionSize(
         identifier=f"transfer_{index}",
-        source_iri=source.instance_iri,
-        destination_iri=target.instance_iri,
-        transfer_device_iri=pipette.instance_iri,
+        participant_source=source.instance_iri,
+        participant_destination=target.instance_iri,
+        participant_device=pipette.instance_iri,
         portion_size=portion_size,
         temporal_cost=time_cost,  # Takes 3 time units
         scheduled_start_time=0.0,  # Earliest start
@@ -79,14 +80,14 @@ def example_simulation():
     transfers[5].required_precedents = [transfers[4].identifier, transfers[7].identifier]
     transfers[6].required_precedents = [transfers[5].identifier]
 
-    sim = ActionSimulation(actions=list(transfers.values()))
+    sim = Simulation(operations=list(transfers.values()))
 
     sim.run()
 
     out_file = __file__.replace(".py", ".ttl")
     KnowledgeGraph.graph().serialize(destination=out_file, format="turtle")
     logger.info(f"Exported updated knowledge graph to {out_file}")
-    sim.export_event_log("action_history.csv")
+    sim.export_event_log(__file__.replace(".py", ".csv"))
 
 
 if __name__ == "__main__":
